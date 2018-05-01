6d5e5f4b197af9ed9a7ce8e87b3da33f8e8d11de
Tu Dat Nguyen <nguyentudat@gmail.com>
2018-05-01 00:00:51 -0700

    define models (login, register, dbcontext) for authentication

---
+++ Blog/Models/Account/IdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Account
+{
+    public class IdentityContext : DbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
